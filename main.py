66a86487efe7f060e0b6632fb8670875d1aa383b
Romas <Romashka951@gmail.com>
2020-03-01 18:18:03 +0500

    Доработан вывод в консоль, все адреса пишутся в глобальную переменную

--- main.py
+++ main.py
@@ -6,22 +6,25 @@ import time
 
 async def ip_ping(ip_addr):
     global num_ip
+    num_ip = [[] * i for i in range(4)]
     try:
         result = await asyncio.open_connection(ip_addr, 80)
         if result:
+            num_ip[0].append(ip_addr)
             print("ip адрес %s доступен" % ip_addr)
-    # except ConnectionError:
-    #     print("Задынный порт недоступен у адреса %s" % ip_addr)
-    # except OSError:
-    #     print("Превышен лимит ожиданий у %s" % ip_addr)
-    # except (asyncio.LimitOverrunError, asyncio.IncompleteReadError):
-    #     print("Неясно что тут у %s" % ip_addr)
+    except ConnectionError:
+        num_ip[1].append(ip_addr)
+        print("Задынный порт недоступен у адреса %s" % ip_addr)
+    except OSError:
+        num_ip[2].append(ip_addr)
+        print("Превышен лимит ожиданий у %s" % ip_addr)
     except:
-        pass
-    num_ip = num_ip + 1
+        num_ip[3].append(ip_addr)
+        print("Неясно что тут у %s" % ip_addr)
 
 
-num_ip = 0
+start = time.monotonic()
+
 addr = []
 with open('ipaddr.csv', newline='') as csvfile:
     string = csv.reader(csvfile)
@@ -36,11 +39,12 @@ with open('ipaddr.csv', newline='') as csvfile:
             else:
                 addr.append(row[0])
 loop = asyncio.get_event_loop()
-print(f"started at {time.strftime('%X')}")
 task_list = [loop.create_task(ip_ping(ip_ad)) for ip_ad in addr]
 loop.run_until_complete(asyncio.wait(task_list))
-print(loop.time())
-print(f"finished at {time.strftime('%X')}")
-print(
-    'Количество опрошенных адресов: %i и количество прошедших %i' % (len(addr),
-                                                                     num_ip))
+result = time.monotonic() - start
+print("Program time: {:>.3f}".format(result) + " seconds.")
+print('Количество опрошенных адресов: %i' % (len(addr)))
+print('Доступные    |    Недоступные    |    Состояние не установленно  |   Ошибка с сокетами')
+print("{:<20} {:<20} {:<30} {:<20}".format(len(num_ip[0]), len(num_ip[1]),
+                                           len(num_ip[2]), len(num_ip[3])))
+
